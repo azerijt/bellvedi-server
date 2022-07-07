f7b96b798b48167b0f9fe626ffd5f0cfb63de5b6
azerijt <azjuliantemir@gmail.com>
2022-07-07 21:49:33 +0100

    server

--- server.ts
+++ server.ts
@@ -26,10 +26,35 @@ app.use(cors()) //add CORS support to each following route handler
 const client = new Client(dbConfig);
 client.connect();
 
-app.get("/", async (req, res) => {
-  const dbres = await client.query('select * from categories');
-  res.json(dbres.rows);
+
+
+app.get("/stations", async (req, res) => {
+  try{
+    const dbres = await client.query('select from_tiploc, to_tiploc, distance from tracks');
+    res.json(dbres.rows);
+  }
+  catch (error){
+    res.status(404)
+    console.error(error)
+  }
+
 });
+
+// app.get("/closest/:station1/:station2", async (req, res)=>{
+//   try{
+//     const station1 = req.params.station1;
+//     const station2 = req.params.station2;
+//     const dbres = await client.query(`select from_tiploc, to_tiploc, distance from tracks where from_tiploc = ${station1} or to_tiploc = ${station2} order by from_tiploc
+//     '`);
+//     res.json(dbres.rows);
+//   }/pcs/click
+//   catch (error){
+//     res.status(404)
+//     console.error(error)  
+//   }
+// });
+
+
 
 
 //Start the server on the given port
